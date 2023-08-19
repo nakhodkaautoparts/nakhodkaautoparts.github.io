66c3c117b46e93f5ec65a0b149cd821f4101b2d4
nataliausoltseva <usolnatalia@gmail.com>
2023-08-19 19:52:17 +1200

    Add breakpoint styles to the filter

--- nakhodkaautoparts/src/pages/Catalog.tsx
+++ nakhodkaautoparts/src/pages/Catalog.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {Card, Col, Layout, Radio, Row, Select} from 'antd';
 import {Content} from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
@@ -84,83 +84,63 @@ const Catalog = () => {
     }, [optionUpDown, optionFrontBack, optionLeftRight, filterOption]);
 
     return (
-        <Layout style={{paddingTop: '4vh'}}>
+        <Layout style={{paddingTop: '4vh'}} css={layoutStyle}>
             <Helmet>
                 <title>Каталог - Автозапчасти в Находке</title>
             </Helmet>
-            <Sider width="30%" theme={"light"} style={{
-                boxShadow: '5px 8px 24px 5px rgba(0, 0, 0, 0.1)',
-                borderRadius: '8px',
-                marginLeft: '50px',
-                height: '85vh'
-            }}>
-                <Col style={{padding: "20px"}}>
-                    <Row><label style={{fontSize: '20px'}}>Марка</label></Row>
-                    <Row>
-                        <Select
-                            style={{width: '60%', margin: '20px'}}
-                            size={"large"}
-                            placeholder="Марка"
-                            onChange={(value) => {
-                                setSelectedMake(makes.find((make) => make.key === parseInt(value))!);
-                                setSelectedModel(undefined);
-                            }}
-                            value={selectedMake?.label || undefined}
-                        >
-                            {makes.map((make) => (
-                                <Select.Option key={make.key} value={make.key}>
-                                    {make.label}
-                                </Select.Option>
-                            ))}
-                        </Select>
-                    </Row>
-                    <Row><label style={{fontSize: '20px'}}>Модель</label></Row>
-                    <Row>
-                        <Select
-                            disabled={!selectedMake || selectedMake?.key === 0}
-                            style={{width: '60%', margin: '20px'}}
-                            size={"large"}
-                            placeholder="Модель"
-                            onChange={(value) => {
-                                setSelectedModel(selectedMake?.models.find((model) => model.key === parseInt(value))!)
-                                setSelectedYear(undefined);
-                                setSelectedEngine(undefined);
-                            }}
-                            value={selectedModel?.label || undefined}
-                        >
-                            {selectedMake?.models.map((model) => (
-                                <Select.Option key={model.key} value={model.key}>
-                                    {model.label}
-                                </Select.Option>
-                            ))}
-                        </Select>
-                    </Row>
-                    <Row><label style={{fontSize: '20px'}}>Год</label></Row>
-                    <Row>
-                        <Select
-                            disabled={!selectedModel || selectedModel?.key === 0}
-                            style={{width: '60%', margin: '20px'}}
-                            size={"large"}
-                            placeholder="Год"
-                            onChange={(value) => {
-                                setSelectedYear(selectedModel?.year?.find((year) => year.key === parseInt(value))!)
-                            }}
-                            value={selectedYear?.label || undefined}
-                        >
-                            {selectedModel?.year?.map((year) => (
-                                <Select.Option key={year.key} value={year.key}>
-                                    {year.label}
-                                </Select.Option>
-                            ))}
-                        </Select>
-                    </Row>
-                    <Row style={{ justifyContent: "space-between"}}>
-                        <div style={{ display: "flex", flexDirection: 'column', flexGrow: 1 }}>
-                            <Row><label style={{fontSize: '20px'}}>Кузов</label></Row>
+            <div css={siderContainerStyle}>
+                <Sider theme={"light"} css={siderStyle} width={'100%'}>
+                    <Col css={menuContainerStyle}>
+                        <div css={dropdownContainerStyle}>
+                            <Row><label css={labelStyle}>Марка</label></Row>
+                            <Row>
+                                <Select
+                                    size={"large"}
+                                    placeholder="Марка"
+                                    onChange={(value) => {
+                                        setSelectedMake(makes.find((make) => make.key === parseInt(value))!);
+                                        setSelectedModel(undefined);
+                                    }}
+                                    value={selectedMake?.label || undefined}
+                                    css={dropdownStyle}
+                                >
+                                    {makes.map((make) => (
+                                        <Select.Option key={make.key} value={make.key}>
+                                            {make.label}
+                                        </Select.Option>
+                                    ))}
+                                </Select>
+                            </Row>
+                        </div>
+                        <div css={dropdownContainerStyle}>
+                            <Row><label css={labelStyle}>Модель</label></Row>
+                            <Row>
+                                <Select
+                                    disabled={!selectedMake || selectedMake?.key === 0}
+                                    css={dropdownStyle}
+                                    size={"large"}
+                                    placeholder="Модель"
+                                    onChange={(value) => {
+                                        setSelectedModel(selectedMake?.models.find((model) => model.key === parseInt(value))!)
+                                        setSelectedYear(undefined);
+                                        setSelectedEngine(undefined);
+                                    }}
+                                    value={selectedModel?.label || undefined}
+                                >
+                                    {selectedMake?.models.map((model) => (
+                                        <Select.Option key={model.key} value={model.key}>
+                                            {model.label}
+                                        </Select.Option>
+                                    ))}
+                                </Select>
+                            </Row>
+                        </div>
+                        <div css={dropdownContainerStyle}>
+                            <Row><label css={labelStyle}>Кузов</label></Row>
                             <Row>
                                 <Select
                                     disabled={!selectedModel || selectedModel?.key === 0}
-                                    style={{width: '60%', margin: '20px'}}
+                                    css={dropdownStyle}
                                     size={"large"}
                                     placeholder="Кузов"
                                     onChange={(value) => {
@@ -176,12 +156,12 @@ const Catalog = () => {
                                 </Select>
                             </Row>
                         </div>
-                        <div style={{ display: "flex", flexDirection: 'column', flexGrow: 1 }}>
-                            <Row><label style={{fontSize: '20px'}}>Двигатель</label></Row>
+                        <div css={dropdownContainerStyle}>
+                            <Row><label css={labelStyle}>Двигатель</label></Row>
                             <Row>
                                 <Select
                                     disabled={!selectedModel || selectedModel?.key === 0}
-                                    style={{width: '60%', margin: '20px'}}
+                                    css={dropdownStyle}
                                     size={"large"}
                                     placeholder="Двигатель"
                                     onChange={(value) => {
@@ -197,37 +177,58 @@ const Catalog = () => {
                                 </Select>
                             </Row>
                         </div>
-                    </Row>
-                    <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        <Row><label style={{fontSize: '20px'}}>Расположение</label></Row>
-                        <Radio.Group
-                            options={optionsFrontBack}
-                            onChange={(e) => setOptionFrontBack({ value: e.target.value })}
-                            value={optionFrontBack.value}
-                            optionType="button"
-                            buttonStyle="solid"
-                            style={{ display: 'flex', padding: '20px', paddingBottom: '0' }}
-                        />
-                        <Radio.Group
-                            options={optionsLeftRight}
-                            onChange={(e) => setOptionLeftRight({ value: e.target.value })}
-                            value={optionLeftRight.value}
-                            optionType="button"
-                            buttonStyle="solid"
-                            style={{ display: 'flex', padding: '20px', paddingBottom: '0' }}
-                        />
-                        <Radio.Group
-                            options={optionsUpDown}
-                            onChange={(e) => setOptionUpDown({ value: e.target.value })}
-                            value={optionUpDown.value}
-                            optionType="button"
-                            buttonStyle="solid"
-                            style={{ display: 'flex', padding: '20px', paddingBottom: '0' }}
-                        />
-                    </div>
-                </Col>
-            </Sider>
-            <Content style={{padding: '0 50px '}}>
+                        <div css={dropdownContainerStyle}>
+                            <Row><label css={labelStyle}>Год</label></Row>
+                            <Row>
+                                <Select
+                                    disabled={!selectedModel || selectedModel?.key === 0}
+                                    css={dropdownStyle}
+                                    size={"large"}
+                                    placeholder="Год"
+                                    onChange={(value) => {
+                                        setSelectedYear(selectedModel?.year?.find((year) => year.key === parseInt(value))!)
+                                    }}
+                                    value={selectedYear?.label || undefined}
+                                >
+                                    {selectedModel?.year?.map((year) => (
+                                        <Select.Option key={year.key} value={year.key}>
+                                            {year.label}
+                                        </Select.Option>
+                                    ))}
+                                </Select>
+                            </Row>
+                        </div>
+                        <div css={dropdownContainerStyle}>
+                            <Row><label css={labelStyle}>Расположение</label></Row>
+                            <Radio.Group
+                                options={optionsFrontBack}
+                                onChange={(e) => setOptionFrontBack({ value: e.target.value })}
+                                value={optionFrontBack.value}
+                                optionType="button"
+                                buttonStyle="solid"
+                                css={radioStyle}
+                            />
+                            <Radio.Group
+                                options={optionsLeftRight}
+                                onChange={(e) => setOptionLeftRight({ value: e.target.value })}
+                                value={optionLeftRight.value}
+                                optionType="button"
+                                buttonStyle="solid"
+                                css={radioStyle}
+                            />
+                            <Radio.Group
+                                options={optionsUpDown}
+                                onChange={(e) => setOptionUpDown({ value: e.target.value })}
+                                value={optionUpDown.value}
+                                optionType="button"
+                                buttonStyle="solid"
+                                css={radioStyle}
+                            />
+                        </div>
+                    </Col>
+                </Sider>
+            </div>
+            <Content css={rightSectionStyle}>
                 <h2>Модели</h2>
                 <div className="site-layout-content">
                     <Row gutter={16}>
@@ -281,4 +282,86 @@ const partsHeaderStyle = css`
 
 const partLabelStyle = css`
   text-transform: capitalize;
+`;
+
+const siderContainerStyle = css`
+  height: 85vh;
+  width: 30%;
+  margin-left: 50px;
+
+  @media (max-width: 1200px) {
+    height: 100%;
+    width: 80%;
+    margin: auto;
+  }
+`;
+
+const siderStyle = css`
+  box-shadow: 5px 8px 24px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+`;
+
+const menuContainerStyle = css`
+  padding: 20px;
+  @media (max-width: 1200px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 780px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 600px) {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+const layoutStyle = css`
+  @media (max-width: 1200px) {
+    display: flex;
+    flex-direction: column !important;
+  }
+`;
+
+const rightSectionStyle = css`
+  padding: 0 50px;
+  @media (max-width: 1200px) {
+    width: 100% !important;
+  }
+`;
+
+const labelStyle = css`
+  font-size: 20px;
+
+  @media (max-width: 1200px) {
+    font-size: 14px;
+  }
+`;
+
+const dropdownContainerStyle = css`
+  display: flex;
+  flex-direction: column;
+  
+`;
+
+const dropdownStyle = css`
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+
+  @media (max-width: 1200px) {
+    margin-right: 20px;
+  }
+`;
+
+const radioStyle = css`
+  display: flex;
+  padding: 20px 0 0;
+  >label {
+    flex-grow: 1;
+    flex-basis: 0;
+  }
 `;

e059d94c60ebe2eff28bdb84087df17d1161481c
nataliausoltseva <usolnatalia@gmail.com>
2023-08-27 09:29:36 +1200

    Add GA4 to index html

--- nakhodkaautoparts/src/App.tsx
+++ nakhodkaautoparts/src/App.tsx
@@ -1,49 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes, useLocation } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
 import Catalog from "./pages/Catalog";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import { Header } from "antd/es/layout/layout";
 import { Navbar } from "./components/Navbar";
 import { Layout } from "antd";
-import ReactGA from "react-ga4";
-
-const PATHS_TO_PAGES = [
-    {
-        path: '/',
-        title: 'Home'
-    },
-    {
-        path: '/catalog',
-        title: 'Catalog'
-    },
-    {
-        path: '/contact',
-        title: 'Contact'
-    },
-];
 
 function App() {
-    const [isSet, setIsSet] = useState(false);
-    const location = useLocation();
-
-    useEffect(() => {
-        if (!isSet && process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
-            ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
-            setIsSet(true);
-        }
-    }, [isSet]);
-
-    useEffect(() => {
-        if (isSet) {
-            ReactGA.send({
-                hitType: "pageview",
-                page: location.pathname,
-                title: PATHS_TO_PAGES.find(path => path.path === location.pathname)?.title
-            });
-        }
-    }, [isSet, location]);
-
     return (
         <Layout style={{height: '100%'}}>
             <Header style={{paddingInline: 0}}><Navbar/></Header> {/* TODO: better solution for padding? maybe fine */}
